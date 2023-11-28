766df13b3bfdec4ae7f28e5acdef0dba40ddb101
Ikaguia <Ikaguia@users.noreply.github.com>
2023-11-28 13:27:34 -0300

    Nameswap

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 import {initConfig} from "./config.js";
 import { registerSettings } from "./settings.js";
 
-export const MODULE_ID = "enhancedcombathud-dnd5e";
+export const MODULE_ID = "enhancedcombathud-sw5e";
 
 Hooks.on("setup", () => {
     registerSettings();
